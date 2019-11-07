0c361699df717ead0caf554336ad60afa13ae04b
Misty Bui <mishtybui@gmail.com>
2019-11-07 13:23:09 +0200

    express app, get / and get /demo

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const express = require('express');
+const app = express();
+
+app.get('/',(request,response)=>{
+    response.send('Hello from my Node server');
+});
+
+app.get('/demo', (request,response)=>{
+    console.log('request',request);
+    response.send('demo')
+});
+
+app.listen(3000, ()=>{
+    console.log('server app start?')
+});
