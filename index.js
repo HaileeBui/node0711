62840c8ebbdf8636c09eb9de4490577503065309
Misty Bui <mishtybui@gmail.com>
2019-11-15 14:02:24 +0200

    database connection and SELECT *

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 'use strict';
 
+require('dotenv').config();
+
 const express = require('express');
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
+
+
 const app = express();
 
 app.use(express.static('public'));
+
+app.get('/animal',(req,res) =>{
+    connection.query(
+        'SELECT * FROM animal',
+        (err,results,fields) => {
+            console.log(results);
+            console.log(fields);
+            res.json(results);
+        }
+    )
+})
 
 
 app.get('/',(request,response)=>{
