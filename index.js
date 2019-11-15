48fd0b1fe00bc2ade099a8b09f469f85a432ebd1
Misty Bui <mishtybui@gmail.com>
2019-11-15 12:57:04 +0200

    serving static files

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
+
 app.get('/',(request,response)=>{
     response.send('Hello from my Node server');
 });
@@ -15,3 +18,5 @@ app.get('/demo', (request,response)=>{
 app.listen(3000, ()=>{
     console.log('server app start?')
 });
+
+
