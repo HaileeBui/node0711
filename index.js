950131a5130f74923fcda840d0c374e8d31c721c
Misty Bui <mishtybui@gmail.com>
2019-11-18 15:04:06 +0200

    SQL in seperated module.exports

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 require('dotenv').config();
 
 const express = require('express');
-const connection = require('./model/db.js');
 const bodyParser = require('body-parser');
+const animal = require('./model/animal');
 
 
 const app = express();
@@ -13,11 +13,8 @@ app.use(express.static('public'));
 
 app.get('/animals', async (req,res) =>{
     try {
-    const [results, fields]= await connection.promise().query(
-        'SELECT * FROM animal');
-            console.log(results);
-            console.log(fields);
-            res.json(results);  
+        const results= await animal.getAll() ;
+        res.json(results);
     }catch (e){
         console.log(e);
         res.send('db error');
@@ -28,26 +25,19 @@ app.get('/animal', async (req,res)=>{
     console.log(req.query);
     //res.send(`query param? ${req.query}`);
     try{
-        const [results] = await connection.query(
-            'SELECT * FROM animal WHERE name LIKE ?',
-            [req.query.name]);
-        res.json(results);
+        res.json= await animal.search(req.query.name);
     }catch(e){
-        res.send(`db error ${e}`);
+        res.send(`db error`);
     }
 });
 
 app.post('/animal', bodyParser.urlencoded({extended:true}), async (req,res)=>{
     console.log(req.body);
     try{
-        const [result] = await connection.query(
-            'INSERT INTO animal (name) VALUES (?)',
-            [req.body.name]
-        );
-        res.json(result);
+        res.json(animal.insert(req.body.name));
     }catch(e){
         console.log(e);
-        res.send('db error');
+        throw 'db error';
     }
 });
 
