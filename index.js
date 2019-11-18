95277dcc9d161d37b53cd203280f58a81fb2fda8
Misty Bui <mishtybui@gmail.com>
2019-11-18 14:18:49 +0200

    body parser extended:true

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.get('/animal', async (req,res)=>{
     }
 });
 
-app.post('/animal', bodyParser.urlencoded(), async (req,res)=>{
+app.post('/animal', bodyParser.urlencoded({extended:true}), async (req,res)=>{
     console.log(req.body);
     try{
         const [result] = await connection.query(
